24fee1b550dcbdab10f2e7c48a5153fd0d91b110
Yatharth Kaushal <kaushalyatharth@gmail.com>
2024-07-25 15:13:19 +0530

    tailwind and daisyui setup

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -6,6 +6,25 @@ export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
+        <h1 className="p-10 text-center font-extrabold">
+          TESTING TAILWIND SETUP
+        </h1>
+        <div role="alert" className="alert alert-success">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6 shrink-0 stroke-current"
+            fill="none"
+            viewBox="0 0 24 24"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+            />
+          </svg>
+          <span>DAISY UI SETUP SUCCESS</span>
+        </div>
         <Image
           className={styles.logo}
           src="/next.svg"
